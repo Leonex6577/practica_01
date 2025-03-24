09a16625853f524c0872f6ca43d179e953be5de9
Leonex6577 <ale422515@gmail.com>
2025-03-24 16:45:44 -0600

    Funciones

---
+++ Funciones/practica_01.py
@@ -0,0 +1,30 @@
+nume1 = int(input("1:"))
+nume2 = int(input("2:"))
+
+def suma(num1, num2):
+    sumat = num1 + num2 
+    print(f"La suma es {sumat}")
+    
+if __name__ == "__main__":
+    suma(nume1, nume2)
+    
+def resta(num1, num2):
+    rest = num1 - num2
+    print(f"La resta es {rest}")
+    
+if __name__ == "__main__":
+    resta(nume1, nume2)
+    
+def multiplicacion(num1, num2):
+    multipli = num1 * num2
+    print(f"La multiplicacion es {multipli}")
+    
+if __name__ == "__main__":
+    multiplicacion(nume1, nume2)
+    
+def division(num1, num2):
+    divit = num1 / num2
+    print(f"La division es {divit}")
+    
+if __name__ == "__main__":
+    division(nume1, nume2)
